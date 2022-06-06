4904dbbbc4e4af07c9d7ac47d0b975f40d5cdb9c
Manfred-Charizard <pedtogabriela@gmail.com>
2022-06-06 17:22:21 -0300

    comite three

--- api/src/repository/repository.js
+++ api/src/repository/repository.js
@@ -1,13 +1,13 @@
 import { con } from './connection.js'
 
 
-
-export async function buscar(nome) {
-    const comando = 
-        `select id_anime 		id,
-                nm_anime		nome,
-           from tb_lista`
+export async function buscar(id) {
+    const comando =
+        `SELECT id_anime		id,
+                nm_anime		nome
+           FROM tb_lista
+          WHERE id_anime = ? `;
     
-    const [linhas] = await con.query(comando, [nome])
+    const [linhas] = await con.query(comando, [id]);
     return linhas[0];
 }
